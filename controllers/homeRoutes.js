1759a7c8c5d19ace5b521467001c6ebe2b999e03
UnDuhDuhSea <tafimba@gmail.com>
2021-04-27 17:28:35 -0700

    Updating post routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     // Get all Posts and JOIN with user data
     const postData = await Post.findAll({
